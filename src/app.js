33bd6a763d9f6572c76225018f1ef6fb1210a8ab
moat-dib <kharin-m@yandex.ru>
2024-10-27 13:24:40 +0300

    new module

--- src/app.js
+++ src/app.js
@@ -1,16 +1,19 @@
 import './styles.css'
 import './soundstyles.css'
+import './pianostyles.css'
 
 import { ContextMenu } from './menu';
 import { BackgroundModule } from './modules/background.module';
 import { ShapeModule } from './modules/shape.module';
 import { SoundModule } from './modules/sound.module';
+import { PianoModule } from './modules/piano.module';
 
 const contextMenu = new ContextMenu('#menu');
 
 contextMenu.add(new BackgroundModule()); 
 contextMenu.add(new ShapeModule());
 contextMenu.add(new SoundModule());
+contextMenu.add(new PianoModule());
 //Можно дописать в аргументы остальные модули через запятую
 // Пример: contextMenu.add(backgroundModule, createObject, customModule...); 
 
