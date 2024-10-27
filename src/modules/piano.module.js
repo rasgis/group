a7ef1fb632a07f5a7ccd27b9130aa68eea2434bf
moat-dib <kharin-m@yandex.ru>
2024-10-27 20:52:27 +0300

    refactoring

--- src/modules/piano.module.js
+++ src/modules/piano.module.js
@@ -1,45 +1,44 @@
 import {Module} from '../core/module'
 
 export class PianoModule extends Module {
-
     constructor(type, text) {
         //Указал type - id li-шки и содержимое li-шки
         super('SimplePiano', 'Пианино');
-      }
+    }
   
 renderElements() {
     if (document.querySelector('.wrapper') === null) {
-    const link = document.createElement('link');
-    link.rel = 'stylesheet';
-    link.href = 'pianostyles.css';
-    document.head.appendChild(link);
-    const $body = document.querySelector('body');
-    const $divwrapper = this.createElement('div','wrapper',null);
-    const $h1 = this.createElement('h1',null,'Piano for hakaton');
-    $divwrapper.appendChild($h1);
-    const $closeButton = this.createElement('button','close-button','X');
-    $divwrapper.appendChild($closeButton);
-    const $ul = this.createElement('ul','piano-keys',null);
-    $ul.innerHTML = 
-    '<li class="key white" data-key="a"></li>'+
-    '<li class="key black" data-key="w"></li>'+
-    '<li class="key white" data-key="s"></li>'+
-    '<li class="key black" data-key="e"></li>'+
-    '<li class="key white" data-key="d"></li>'+
-    '<li class="key white" data-key="f"></li>'+
-    '<li class="key black" data-key="t"></li>'+
-    '<li class="key white" data-key="g"></li>'+
-    '<li class="key black" data-key="y"></li>'+
-    '<li class="key white" data-key="h"></li>'+
-    '<li class="key black" data-key="u"></li>'+
-    '<li class="key white" data-key="j"></li>'+
-    '<li class="key white" data-key="k"></li>'+
-    '<li class="key black" data-key="o"></li>'+
-    '<li class="key white" data-key="l"></li>'+
-    '<li class="key black" data-key="p"></li>'+
-    '<li class="key white" data-key=";"></li>';
-    $divwrapper.appendChild($ul);
-    $body.appendChild($divwrapper);
+        const link = document.createElement('link');
+        link.rel = 'stylesheet';
+        link.href = 'pianostyles.css';
+        document.head.appendChild(link);
+        const $body = document.querySelector('body');
+        const $divwrapper = this.createElement('div','wrapper',null);
+        const $h1 = this.createElement('h1',null,'Piano for hakaton');
+        $divwrapper.appendChild($h1);
+        const $closeButton = this.createElement('button','close-button','X');
+        $divwrapper.appendChild($closeButton);
+        const $ul = this.createElement('ul','piano-keys',null);
+        $ul.innerHTML = 
+            '<li class="key white" data-key="a"></li>'+
+            '<li class="key black" data-key="w"></li>'+
+            '<li class="key white" data-key="s"></li>'+
+            '<li class="key black" data-key="e"></li>'+
+            '<li class="key white" data-key="d"></li>'+
+            '<li class="key white" data-key="f"></li>'+
+            '<li class="key black" data-key="t"></li>'+
+            '<li class="key white" data-key="g"></li>'+
+            '<li class="key black" data-key="y"></li>'+
+            '<li class="key white" data-key="h"></li>'+
+            '<li class="key black" data-key="u"></li>'+
+            '<li class="key white" data-key="j"></li>'+
+            '<li class="key white" data-key="k"></li>'+
+            '<li class="key black" data-key="o"></li>'+
+            '<li class="key white" data-key="l"></li>'+
+            '<li class="key black" data-key="p"></li>'+
+            '<li class="key white" data-key=";"></li>';
+        $divwrapper.appendChild($ul);
+        $body.appendChild($divwrapper);
     }
 }
 createElement (element, className, textContent) {
@@ -57,25 +56,25 @@ createElement (element, className, textContent) {
     return $element;
 }
 makeKeysAlive() {
-   const pianoKeys = document.querySelectorAll(".piano-keys .key");
-let allKeys = [],
-audio = new Audio('/modules/tunes/a.wav'); 
+    const pianoKeys = document.querySelectorAll(".piano-keys .key");
+    let allKeys = [],
+    audio = new Audio('/modules/tunes/a.wav'); 
 
-const playTune = (key) => {
-    audio.src = `./src/modules/tunes/${key}.wav`; 
-    audio.play();
-    const clickedKey = document.querySelector(`[data-key="${key}"]`); 
-    clickedKey.classList.add("active"); 
-    setTimeout(() => {clickedKey.classList.remove("active");}, 350);
-}
-pianoKeys.forEach(key => {
-    allKeys.push(key.dataset.key); 
-    key.addEventListener("click", () => playTune(key.dataset.key));
-});
-const pressedKey = (e) => {
-    if(allKeys.includes(e.key)) playTune(e.key);
-}
-document.addEventListener("keydown", pressedKey);
+    const playTune = (key) => {
+        audio.src = `./src/modules/tunes/${key}.wav`; 
+        audio.play();
+        const clickedKey = document.querySelector(`[data-key="${key}"]`); 
+        clickedKey.classList.add("active"); 
+        setTimeout(() => {clickedKey.classList.remove("active");}, 350);
+    }
+    pianoKeys.forEach(key => {
+        allKeys.push(key.dataset.key); 
+        key.addEventListener("click", () => playTune(key.dataset.key));
+    });
+    const pressedKey = (e) => {
+        if(allKeys.includes(e.key)) playTune(e.key);
+    }
+    document.addEventListener("keydown", pressedKey);
 }
 trigger() {
     this.renderElements();
@@ -88,5 +87,5 @@ trigger() {
             $link.remove();
         }
     });
-}
+    }
 }
