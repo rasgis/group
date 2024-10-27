6f90e5bcb4f29db9fe00bdf674679940d317df3a
Дарья Провальская <kaamomee@gmail.com>
2024-10-27 17:37:12 +0300

    add TimerModule

--- src/utils.js
+++ src/utils.js
@@ -9,8 +9,43 @@ export function createResultClicksBlock(clickCount) {
   return resultClicks;
 }
 
-export function removeResultClicksBlock(result){
-  setTimeout(()=>{
-    result.remove()
-  }, 5000)
+export function removeResultClicksBlock(result) {
+  setTimeout(() => {
+    result.remove();
+  }, 5000);
+}
+
+export function startTimer(seconds, id) {
+  const timerBlock = createTimerBlock();
+  document.body.appendChild(timerBlock);
+
+  id = setInterval(() => {
+    if (seconds < 0) {
+      timerBlock.remove();
+      clearInterval(id);
+      const endMessage = createEndTimerMessage();
+      document.body.appendChild(endMessage);
+      setTimeout(() => {
+        endMessage.remove();
+      }, 5000);
+      return;
+    }
+
+    timerBlock.textContent = `Оставшееся время: ${seconds}`;
+    seconds -= 1;
+  }, 1000);
+}
+
+function createTimerBlock() {
+  const timerContainer = document.createElement("div");
+  timerContainer.className = "timer-container";
+
+  return timerContainer;
+}
+
+function createEndTimerMessage() {
+  const endTimerMessage = document.createElement("h1");
+  endTimerMessage.textContent = `Время вышло!`;
+
+  return endTimerMessage;
 }
